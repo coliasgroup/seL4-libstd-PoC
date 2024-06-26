0acd591a552db791c2b37bc3a04d855bf7a9ee4a
Nick Spinale <nick@nickspinale.com>
2024-06-26 11:42:09 +0000

    x

--- nix/scope/default.nix
+++ nix/scope/default.nix
@@ -1,5 +1,7 @@
 { lib
 , runCommandNoCC
+, symlinkJoin
+, lndir
 }:
 
 self: super: with self; {
@@ -16,8 +18,7 @@ self: super: with self; {
 
   patchToolchainSrc = toolchain: src:
     let
-    in
-      runCommandNoCC "${toolchain.name}-patched" {} ''
+      x = runCommandNoCC "${toolchain.name}-patched" {} ''
         mkdir -p $out/bin $out/lib $out/lib/rustlib/src
 
         for f in $(find ${toolchain}/bin -type f -mindepth 1 -maxdepth 1 -printf '%P\n'); do
@@ -47,7 +48,56 @@ self: super: with self; {
             done
           fi
         done
-      ''
+      '';
+      y = runCommandNoCC "${toolchain.name}-patched" {
+        nativeBuildInputs = [ lndir ];
+      } ''
+        mkdir -p $out
+        lndir -silent ${toolchain} $out
+
+        for f in $(find ${toolchain}/bin -type f -mindepth 1 -maxdepth 1 -printf '%P\n'); do
+          install ${toolchain}/bin/$f $out/bin
+
+          if [ $f != rustfmt ]; then
+            continue
+          fi
+
+          if isELF $out/bin/$f; then
+            patchelf --set-rpath $out/lib $out/bin/$f || true
+          fi
+        done
+
+        for file in $(find ${toolchain}/lib -name 'librustc_driver-*'); do
+          install $file $out/lib
+        done
+
+        rm -r $out/lib/rustlib/src/rust
+        ln -s ${src} $out/lib/rustlib/src/rust
+      '';
+      z = runCommandNoCC "${toolchain.name}-patched" {} ''
+        cp -r --no-preserve=owner,mode ${toolchain} $out
+
+        for f in $(find ${toolchain}/bin -type f -mindepth 1 -maxdepth 1 -printf '%P\n'); do
+          install ${toolchain}/bin/$f $out/bin
+
+          if [ $f != rustfmt ]; then
+            continue
+          fi
+
+          if isELF $out/bin/$f; then
+            patchelf --set-rpath $out/lib $out/bin/$f || true
+          fi
+        done
+
+        for file in $(find ${toolchain}/lib -name 'librustc_driver-*'); do
+          install $file $out/lib
+        done
+
+        rm -r $out/lib/rustlib/src/rust
+        cp -r ${src} $out/lib/rustlib/src/rust
+      '';
+    in
+      z
     ;
 
   patched = rec {
@@ -68,6 +118,7 @@ self: super: with self; {
   sysroot = buildSysroot {
     inherit (patched) rustEnvironment;
     release = false;
+    # std = true;
   };
 
   nativeTest = buildCratesInLayers {
